261dca4d717571decb6fb96fd79579867394bd77
Aibek <aibek.abdykasymov@gmail.com>
2023-04-01 00:45:26 +0600

    Added index and proxy health check lambda services

---
+++ index.test.ts
@@ -0,0 +1,9 @@
+// import {describe, expect, test} from '@jest/globals';
+// import {handler} from './index';
+//
+// describe('Test index service', () => {
+//   test('check successfull result', async () =>{
+//     const result = await  handler({body: ''});
+//     expect(result, {});
+//   });
+// });
