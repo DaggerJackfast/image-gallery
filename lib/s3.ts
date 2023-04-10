b5ec778de93f03fc380b8763959ca7ca81dbf9ac
Aibek <aibek.abdykasymov@gmail.com>
2023-04-10 12:19:01 +0600

    Added using aws-sdk v2 instead of old

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,5 +1,13 @@
-import {PutObjectRequest} from 'aws-sdk/clients/s3';
-import {s3} from '../file/s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import * as AWS from '@aws-sdk/client-s3';
+import {parse} from 'path';
+export const s3 = new AWS.S3({
+  // s3ForcePathStyle: true,
+  // accessKeyId: 'S3RVER', // This specific key is required when working offline
+  // secretAccessKey: 'S3RVER',
+  // signatureVersion: 'v4',
+  // endpoint: new AWS.Endpoint('http://localhost:4569'),
+});
 
 interface ISendParams {
   base64Url: string;
@@ -12,10 +20,16 @@ interface IUploadParams {
   contentType: string;
 }
 
-interface IUpload {
+interface IUploadUrl {
   filename: string;
   url: string;
   uploadUrl: string;
+  expires: Date;
+}
+
+interface IGetUrl {
+  filename: string;
+  url: string;
   expires: Date;
 }
 
@@ -24,41 +38,91 @@ export const sendBase64ToS3 = async (sendParams: ISendParams): Promise<string> =
   const decodedFile = Buffer.from(base64Url.replace(/^data:image\/\w+;base64,/, ''), 'base64');
 
   const BUCKET_NAME = process.env.FILE_BUCKET_NAME || '';
-  const REGION = process.env.AWS_REGION;
-  console.log('AWS REGION ========> : ', REGION);
-  console.log('BUCKET_NAME ========> : ', BUCKET_NAME);
-  const params: PutObjectRequest = {
+  const params = {
     Bucket: BUCKET_NAME,
     Key: filename,
     Body: decodedFile,
     ContentType: contentType
   };
-  const uploadResult = await s3.upload(params).promise();
-  return uploadResult.Location;
+  const command = new AWS.PutObjectCommand(params);
+  const uploadResult = await s3.send(command);
+  console.log('uploadResult: ', JSON.stringify(uploadResult, null, 4));
+  return filename; // TODO: change to uploadResult property
 };
 
-export const getUploadUrl = async (uploadParams: IUploadParams): Promise<IUpload> => {
+const getUniqueName = (fileName: string, datetime: Date, prefix = ''): string => {
+  const extension = parse(fileName).ext;
+  const name = parse(fileName).name;
+  const timestamp = datetime.getTime().toString();
+  return `${prefix}${name}_${timestamp}${extension}`;
+
+};
+
+export const getPresignedUploadUrl = async (uploadParams: IUploadParams): Promise<IUploadUrl> => {
   const expires = 600; // 10 minutes;
   const {filename, contentType} = uploadParams;
   const BUCKET_NAME = process.env.FILE_BUCKET_NAME || '';
-  const REGION = process.env.AWS_REGION;
-  console.log('AWS REGION ========> : ', REGION);
-  console.log('BUCKET_NAME ========> : ', BUCKET_NAME);
+  const REGION = process.env.RUNTIME_REGION;
+  const uniqueFileName = getUniqueName(filename, new Date());
   const s3Params = {
     Bucket: BUCKET_NAME,
-    Key: filename,
+    Key: uniqueFileName,
     // ACL: event.body.isPublic ? 'public-read' : null,
     ContentType: contentType,
-    Expires: expires
   };
-  const uploadUrl = s3.getSignedUrl('putObject', s3Params);
+  const command = new AWS.PutObjectCommand(s3Params);
+  const uploadUrl = await getSignedUrl(s3,command, {expiresIn: expires});
   const expiresDate = new Date();
   expiresDate.setSeconds(expiresDate.getSeconds() + expires);
-  const url = `https://${BUCKET_NAME}.s3.${REGION}.amazonaws.com/${filename}`;
+  const url = `https://${BUCKET_NAME}.s3.${REGION}.amazonaws.com/${uniqueFileName}`;
   return {
     uploadUrl: uploadUrl,
-    filename: filename,
+    filename: uniqueFileName,
     url: url,
     expires: expiresDate
   };
 };
+
+export const getPresignedGetUrl = async (filename: string): Promise<IGetUrl> => {
+  const expires = 600; // 10 minutes
+  const BUCKET_NAME = process.env.FILE_BUCKET_NAME || '';
+  const s3Params = {
+    Bucket: BUCKET_NAME,
+    Key: filename
+  };
+  const command = new AWS.GetObjectCommand(s3Params);
+  const getUrl = await getSignedUrl(s3, command, {expiresIn: expires});
+  const expiresDate = new Date();
+  expiresDate.setSeconds(expiresDate.getSeconds() + expires);
+  return {
+    filename,
+    url: getUrl,
+    expires: expiresDate
+  };
+};
+
+export const checkFileIsExists = async (filename: string): Promise<boolean> => {
+  const BUCKET_NAME = process.env.FILE_BUCKET_NAME || '';
+  const s3Params = {
+    Bucket: BUCKET_NAME,
+    Key: filename,
+  };
+  const command = new AWS.HeadObjectCommand(s3Params);
+  try {
+    const metadata = await s3.send(command);
+    return !!(metadata);
+  }catch(e) {
+    return false;
+  }
+};
+
+export const getFileMetadata = async (filename: string): Promise<unknown> => {
+  const BUCKET_NAME = process.env.FILE_BUCKET_NAME || '';
+  const s3Params = {
+    Bucket: BUCKET_NAME,
+    Key: filename,
+  };
+  const command = new AWS.HeadObjectCommand(s3Params);
+  return await s3.send(command);
+};
+
