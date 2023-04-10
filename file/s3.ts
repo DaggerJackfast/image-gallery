6904fe244239e8d33ca534e7f8f28ebe3fd0640d
Aibek <aibek.abdykasymov@gmail.com>
2023-04-10 12:18:36 +0600

    Deleted unused s3

--- file/s3.ts
+++
@@ -1,7 +0,0 @@
-import AWS from 'aws-sdk';
-export const s3 = new AWS.S3({
-  s3ForcePathStyle: true,
-  accessKeyId: 'S3RVER', // This specific key is required when working offline
-  secretAccessKey: 'S3RVER',
-  endpoint: new AWS.Endpoint('http://localhost:4569'),
-});
