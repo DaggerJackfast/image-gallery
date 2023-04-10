9270e651887e1ec4c9ec191ec74f8f82180ebc05
Aibek <aibek.abdykasymov@gmail.com>
2023-04-10 12:17:45 +0600

    Fixed fileBucket resource

--- resources/fileBucketResources.ts
+++ resources/fileBucketResources.ts
@@ -2,7 +2,9 @@ export default {
   Resources: {
     FileBucket: {
       Type: 'AWS::S3::Bucket',
-      Properties: '${self:custom.FILE_BUCKET_NAME}',
+      Properties: {
+        BucketName: '${self:custom.FILE_BUCKET_NAME}'
+      },
       AccessControl: 'Private',
     }
   }
