8ffbdced7c05711e38373ba5bdb5348276d11292
Aibek <aibek.abdykasymov@gmail.com>
2023-04-11 00:17:35 +0600

    Added delete file when delete query in table

--- handlers/image/destroy.ts
+++ handlers/image/destroy.ts
@@ -1,12 +1,17 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
 import {connectToDatabase} from '../../database/connector';
+import {deleteFile} from '../../lib/s3';
 
 export const destroy = async(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const {Image} = await connectToDatabase();
   const imageId = event.pathParameters?.id;
   const userId = event.requestContext.authorizer?.principalId;
   const image = await Image.findOne({where: {id: imageId, user: userId}});
+  if(!image) {
+    return { statusCode: 404, body: 'Not Found'};
+  }
   await image?.destroy();
+  await deleteFile(image.filename);
   return { statusCode: 204, body: '' };
 };
 
