fcc23759bceb521eb4a54b93f677d2d8fa2f9cfc
Aibek <aibek.abdykasymov@gmail.com>
2023-04-15 00:22:59 +0600

    Added sqs lib util

---
+++ lib/sqs.ts
@@ -0,0 +1,31 @@
+import {GetQueueUrlCommand, SendMessageCommand, SendMessageCommandOutput, SQSClient} from '@aws-sdk/client-sqs';
+
+export const sqs = new SQSClient({
+  region: process.env.RUNTIME_REGION,
+  credentials: {
+    accessKeyId: 'root',
+    secretAccessKey: 'root',
+  },
+  endpoint: 'http://localhost:9324',
+  apiVersion: '2012-11-05'
+});
+
+export const getQueueUrl = async () => {
+  const sqsParams = {
+    QueueName: process.env.QUEUE_NAME
+  };
+  const command = new GetQueueUrlCommand(sqsParams);
+  return await sqs.send(command);
+};
+
+export const sendQueueMessage = async (message: object): Promise<SendMessageCommandOutput> => {
+  const queueUrl = process.env.QUEUE_URL;
+  const sqsParams = {
+    DelaySeconds: 5,
+    MessageBody: JSON.stringify(message),
+    QueueUrl: queueUrl
+  };
+  const command = new SendMessageCommand(sqsParams);
+  return await sqs.send(command);
+};
+
