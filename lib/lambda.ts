702bf4fe5b488e942ab63ddb4cbdda74b7a4ea75
Aibek <aibek.abdykasymov@gmail.com>
2023-04-18 23:50:55 +0600

    Added cors to middyfy helper func

--- lib/lambda.ts
+++ lib/lambda.ts
@@ -1,8 +1,10 @@
 import httpJsonBodyParser from '@middy/http-json-body-parser';
 import middy, {MiddyfiedHandler} from '@middy/core';
+import cors from '@middy/http-cors';
 
 export const middyfy = (handler: MiddyfiedHandler) => {
   return middy(handler)
-    .use(httpJsonBodyParser());
+    .use(httpJsonBodyParser())
+    .use(cors());
   // .use(httpErrorHandler());
 };
