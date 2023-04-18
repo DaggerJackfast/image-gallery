cf13bc13e9d0b80c70783b2d4d3be7829cd6af7c
Aibek <aibek.abdykasymov@gmail.com>
2023-04-18 23:50:31 +0600

    Added origin constant

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,5 @@
 export const UPLOAD_PREFIX = 'uploads';
 export const THUMBNAILS_PREFIX = 'thumbnails';
 export const THUMBNAIL_WIDTH=320;
+
+export const ORIGIN = process.env.NODE_ENV === 'dev' ? '*' : process.env.CORS_ALLOW_ORIGIN;
